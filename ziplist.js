01485a283573c87a424e2f1e776f9b6e7ff787a4
Jayden Huynh <jayden.huynh2509@gmail.com>
2025-02-10 11:15:31 -1000

    Final Commit

---
+++ ziplist.js
@@ -0,0 +1,13 @@
+function zipList(list1, list2) {
+    var result = [];
+    for (var i = 0; i < list1.length; i++) {
+        result.push(list1[i], list2[i]);
+    }
+    return result;
+}
+console.log(zipList([1, 2, 3], ['a', 'b', 'c']));
+// ziplist using functional programming
+function zipListFunctionWay(list1, list2) {
+    return list1.map(function (item, index) { return [item, list2[index]]; }).reduce(function (acc, val) { return acc.concat(val); }, []);
+}
+console.log(zipListFunctionWay([1, 2, 3], ['a', 'b', 'c']));
